0e428b23bfae0eec52a90d754bf3c4bb97fc7691
Michel Calheiros <michelcalheiros@gmail.com>
2018-05-08 11:32:21 -0300

    Submit

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -21,8 +21,47 @@ angular.module('starter.controllers', [])
   $scope.chat = Chats.get($stateParams.chatId);
 })
 
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
+.controller('AccountCtrl', function($scope, $cordovaCamera) {
+
+      $scope.takePhoto = function () {
+        var options = {
+          quality: 75,
+          destinationType: Camera.DestinationType.DATA_URL,
+          sourceType: Camera.PictureSourceType.CAMERA,
+          allowEdit: true,
+          encodingType: Camera.EncodingType.JPEG,
+          targetWidth: 300,
+          targetHeight: 300,
+          popoverOptions: CameraPopoverOptions,
+          saveToPhotoAlbum: false
+      };
+
+          $cordovaCamera.getPicture(options).then(function (imageData) {
+              $scope.imgURI = "data:image/jpeg;base64," + imageData;
+          }, function (err) {
+              // An error occured. Show a message to the user
+          });
+      }
+
+      $scope.choosePhoto = function () {
+        var options = {
+          quality: 75,
+          destinationType: Camera.DestinationType.DATA_URL,
+          sourceType: Camera.PictureSourceType.PHOTOLIBRARY,
+          allowEdit: true,
+          encodingType: Camera.EncodingType.JPEG,
+          targetWidth: 300,
+          targetHeight: 300,
+          popoverOptions: CameraPopoverOptions,
+          saveToPhotoAlbum: false
+      };
+
+          $cordovaCamera.getPicture(options).then(function (imageData) {
+              $scope.imgURI = "data:image/jpeg;base64," + imageData;
+          }, function (err) {
+              // An error occured. Show a message to the user
+          });
+      }
+
+
 });
