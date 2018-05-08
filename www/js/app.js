dad7120020a24076120ee70754883c85910c0df7
Michel Calheiros <michelcalheiros@gmail.com>
2018-05-08 12:16:08 -0300

    Commit

--- www/js/app.js
+++ www/js/app.js
@@ -5,6 +5,9 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
+var controllers = require('./controllers');
+var services = require('./services');
+
 angular.module('starter', ['ionic', 'ngCordova', 'starter.controllers', 'starter.services'])
 
 .run(function($ionicPlatform) {
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,191 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+.controller('DashCtrl', ['$scope', '$stateParams', '$http', '$ionicLoading','$rootScope' ,
+ function($scope, $stateParams, $http, $ionicLoading, $rootScope) {
+
+var placa = "";
+var resposta;
+var carros = 0;
+var sinesp = "";
+$scope.showB = "ng-hide";
+
+$scope.consultarPlaca = function(placa) {
+    $scope.placa = placa;
+    $ionicLoading.show();
+    $scope.sinesp = "0";
+    $scope.showB = "ng-hide";
+    $scope.retorno = "consultarPlaca";
+    $scope.consultaPlacaSinespHTTP();
+}
+
+$scope.consultaPlacaSinespHTTP = function()    {
+    $scope.retorno = "consultaPlacaSinespHTTP";
+    const crypto = require('crypto')
+
+    //$ionicLoading.show();
+
+    const secret = '#8.1.0#Mw6HqdLgQsX41xAGZgsF';
+
+    var lat = Math.floor(Math.random() * (999999 - 100000 + 1)) + 100000;
+    var lon = Math.floor(Math.random() * (999999 - 100000 + 1)) + 100000;
+
+    var latitude  = '-45.5' //. rand(100000, 999999);
+    var longitude = '-15.7' //. rand(100000, 999999);
+
+    latitude = latitude.concat(lat.toString());
+    longitude = longitude.concat(lon.toString());
+
+    placa =  $scope.placa;
+
+    const token = crypto.createHmac('sha1', placa+secret).update(placa).digest('hex');
+    const data = new Date().toISOString().replace("T", " ").substr(0, 19);
+
+    const xml = '<?xml version="1.0" encoding="utf-8" standalone="yes" ?>\
+      <v:Envelope xmlns:v="http://schemas.xmlsoap.org/soap/envelope/">\
+        <v:Header>\
+          <b>samsung GT-I9192</b>\
+          <c>ANDROID</c>\
+          <d>8.1.0</d>\
+          <i>'+latitude+'</i>\
+          <e>4.1.5</e>\
+          <f>10.0.0.1</f>\
+          <g>'+token+'</g>\
+          <k></k>\
+          <h>'+longitude+'</h>\
+          <l>'+data+'</l>\
+          <m>8797e74f0d6eb7b1ff3dc114d4aa12d3</m>\
+        </v:Header>\
+        <v:Body>\
+          <n0:getStatus xmlns:n0="http://soap.ws.placa.service.sinesp.serpro.gov.br/">\
+            <a>'+placa+'</a>\
+          </n0:getStatus>\
+        </v:Body>\
+      </v:Envelope>';
+
+      const headers = {
+        "Content-type": "text/xml",
+        "Accept": "text/xml",
+        "Content-length": xml.length
+      }
+    $scope.retorno = "consultaPlacaSinespHTTPINICIO";
+    $http({
+        method : "POST",
+        url : "https://cidadao.sinesp.gov.br/sinesp-cidadao/mobile/consultar-placa/v3",
+        data: xml,
+        headers: headers
+    }).then(function mySuccess(response) {
+        $scope.sinesp = JSON.stringify(response.data);
+        $scope.consultarPlacaCC();
+    }, function myError(response) {
+        $scope.sinesp = "E";
+        $scope.consultarPlacaCC();
+    });
+}
+
+
+$scope.consultarPlacaCC = function()  {
+    $scope.retorno = "consultarPlacaCC";
+    var dadoscarro = [];
+
+    const headers = {
+        "Content-type": "text/xml",
+        "Accept": "text/xml"
+      }
+
+    $http({
+        method : "POST",
+        url : "http://casarochadois.servehttp.com/fipe/consultasinesp.php?placa=" + $scope.placa,
+        data: $scope.sinesp,
+        headers: headers
+    }).then(function mySuccess(response) {
+        try {
+            $scope.item = 0;
+            $scope.consulta = '';
+            dadoscarro = JSON.stringify(response.data);
+            if (dadoscarro.length > 5) { 
+                $scope.dadoscarro = JSON.parse(dadoscarro);
+                $scope.carros = $scope.dadoscarro.length;
+                if($scope.carros > 1 ){
+                   $scope.showB = "ng-show";
+                }else{
+                    $scope.showB = "ng-hide";
+                }
+                $scope.consultarModeloMR();
+            } else {
+                $scope.showB = "ng-hide";
+                $scope.consulta = 'Placa não Encontrada';
+                $ionicLoading.hide();
+            }
+        }
+        catch(err) {
+            $scope.item = 0;
+            $scope.showB = "ng-hide";
+            $scope.consulta = 'Placa não Encontrada';
+            $ionicLoading.hide();
+        }
+    }, function myError(response) {
+        $ionicLoading.hide();
+        $scope.dadoscarro = null;
+        $scope.carros = 0;
+        $scope.item = 0;
+        $rootScope.dadosmodelomr = null
+        $scope.consulta = 'Placa não Encontrada';
+    });
+}
+
+$scope.consultarModeloMR = function()    {
+    $scope.retorno = "consultarModeloMR";
+    $http({
+        method : "GET",
+        url : "http://casarochadois.servehttp.com/fipe/modelomr.php?fipe="+$scope.dadoscarro[$scope.item].codigo_fipe
+    }).then(function mySuccess(response) {
+        $ionicLoading.hide();
+         try {
+            dadoscarro = JSON.stringify(response.data);
+            $rootScope.dadosmodelomr = JSON.parse(dadoscarro);
+        }
+        catch(err) {
+            $scope.consulta = 'Erro ao consultar Carro Curioso';
+        }
+    }, function myError(response) {
+        $ionicLoading.hide();
+    });
+}
+
+$scope.selecionaCarro = function(dadocarro) {
+    var arr = dadocarro.split('/');
+    for(var i = 0; i < $scope.dadoscarro.length; i++) {
+        if($scope.dadoscarro[i].codigo_fipe == arr[0].trim()){
+            $scope.item = i;
+            $ionicLoading.show();
+            $scope.consultarModeloMR();
+        }
+    }
+}
+
+$scope.habilitaDefinirPlaca = function() {
+    if($scope.carros > 1 ){
+        return 'ng-show';
+    }else{
+        return 'ng-hide';
+    }
+}
+
+$scope.definirPlaca = function()    {
+    $ionicLoading.show();
+    $http({
+        method : "GET",
+        url : "http://casarochadois.servehttp.com/fipe/defineplacafipe.php?placa=" + $scope.placa + "&fipe=" + $scope.dadoscarro[$scope.item].codigo_fipe + "&acao=I"
+    }).then(function mySuccess(response) {
+        $ionicLoading.hide();
+        $scope.consultarPlaca($scope.placa);
+    }, function myError(response) {
+        console.log(response);
+        $ionicLoading.hide();
+    });
+}
+
+}])
 
 .controller('ChatsCtrl', function($scope, Chats) {
   // With the new view caching in Ionic, Controllers are only called
