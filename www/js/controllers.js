fd0846d857c4db7026335a0da1415588559e351a
Michel Calheiros <michelcalheiros@gmail.com>
2018-05-10 10:15:30 -0300

    sobe

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -293,7 +293,12 @@ $scope.takePhotos = function () {
 }
 
   $scope.openALPR = function(imgURI){
-      cordova.plugins.OpenALPR.scan(imgURI, { country: 'br', amount: 3 }, function (results) {
+
+    //cordova.plugins.OpenALPR.scan(imgURI, { country: 'br', amount: 3 }, function (results) {
+
+    var openalpr = cordova.require("cordova/plugin/openalpr");
+
+  openalpr.scan(imgURI, { country: 'br', amount: 3 }, function (results) {
       alert(results);
    }, function (error) {
        alert(error.code + ': ' + error.message)
